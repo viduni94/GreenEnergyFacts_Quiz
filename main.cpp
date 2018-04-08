4907fa8c2f1770b3256a78d7b9963b9aa6d78b83
viduni94 <viduni.ushanka@gmail.com>
2018-04-08 22:38:29 +0530

    Menu interface done

--- main.cpp
+++ main.cpp
@@ -17,4 +17,23 @@ private:
     string answer_d;
 
     int correct_answer;
+
+public:
+    void setValues(string, string, string, string, string, int);
+    void askQuestion();
+
 };
+
+int main()
+{
+    cout << endl;
+    cout << "*********************** Green Energy Facts Quiz MENUv1.0 *************************" << endl;
+    cout << endl;
+    cout << "                              1. Quiz                                             " << endl;
+    cout << "                              2. View Score                                       " << endl;
+    cout << "                              3. Quit                                             " << endl;
+    cout << endl;
+    cout << "**********************************************************************************" << endl;
+//    cout << "Enter an option" << endl;
+//    cout << "Option"
+}
